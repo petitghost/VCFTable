9c9510cf90ac1989a790fd5c4a8fae2372de34e9
petitghost <smallghost617@gmail.com>
2014-07-14 17:32:20 +0800

    created multiple filter objects successfully

--- test/MetaInfoTest.java
+++ test/MetaInfoTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 public class MetaInfoTest{
 	String content =
 			"##fileformat=VCFv4.1\n"+
-			"##FILTER=<ID=LowQual,Description=\"Low quality\">\n";
+			"##FILTER=<ID=LowQual,Description=\"Low quality\">\n"+
+			"##FILTER=<ID=q10,Description=\"Quality below 10\">\n"+
+			"##FILTER=<ID=s50,Description=\"Less than 50% of samples have data\">\n";
 	
 	@Test
 	public void testBasicInfo() {
@@ -16,9 +18,25 @@ public class MetaInfoTest{
 	
 	@Test
 	public void getFilterColumn(){
-		FilterColumn filterThreshold = new FilterColumn(content);
-		assertEquals("LowQual", filterThreshold.id);
-		assertEquals("Low quality", filterThreshold.description);
+		int i=0;
+		FilterColumn[] filterThreshold = new FilterColumn[10];
+		String[] lines=content.split("\n");
+		for(String line:lines){
+			if(line.startsWith("##FILTER")){
+				filterThreshold[i]=new FilterColumn(line);
+				i++;
+			}
+		}
+		
+		assertEquals("LowQual", filterThreshold[0].id);
+		assertEquals("Low quality", filterThreshold[0].description);
+		
+		assertEquals("q10", filterThreshold[1].id);
+		assertEquals("Quality below 10", filterThreshold[1].description);
+		
+		assertEquals("s50", filterThreshold[2].id);
+		assertEquals("Less than 50% of samples have data", filterThreshold[2].description);
 	}
+	
 
 }
